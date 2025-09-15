713b28bd6f0ab81a4f9117bf22542469d8953a09
Stephan Horsthemke <stephan.horsthemke@gmail.com>
2025-09-15 18:54:28 +0200

    debug in prod vol 3

--- shared/config.go
+++ shared/config.go
@@ -18,6 +18,8 @@ func LoadConfig() (*Config, error) {
 		env = "dev" // Default to development if APP_ENV is not set
 	}
 
+	log.Printf("APP_ENV %s", env)
+
 	configFile := fmt.Sprintf("config/%s.json", env)
 	file, err := os.Open(configFile)
 	if err != nil {
@@ -25,12 +27,16 @@ func LoadConfig() (*Config, error) {
 	}
 	defer file.Close()
 
+	log.Printf("congfigfile %s", configFile)
+
+	log.Printf("file %v", file)
+
 	var cfg Config
 	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
 		return nil, fmt.Errorf("failed to parse config file: %w", err)
 	}
 
-	log.Printf("Loaded config with origins: %s", cfg.CorsOrigins)
+	log.Printf("Loaded config with origins: %v", cfg)
 
 	return &cfg, nil
 }
